640f5dac5bfcf8c68381d1f1f7e0970d20f478ea
yminer <miner.yang@broadcom.com>
2025-05-26 00:42:28 +0800

    update routes
    update gitignore

--- utils/api.py
+++ utils/api.py
@@ -1,11 +1,26 @@
 from io import BytesIO
 import openai
-import boto3, logging
+import boto3, logging, httpx, os
 from botocore.exceptions import NoCredentialsError
 
-def make_api_request(client, user_input):
+def botclient():
+    return openai.OpenAI(
+        timeout=60.0,
+        base_url=os.getenv("OPENAI_API_URL"),
+        api_key=os.getenv("OPENAI_API_KEY"),
+        http_client=httpx.Client(verify=False)
+    )
+
+
+def make_api_request(user_input):
+    """
+    Make request to gpt
+
+    :param user_input: User input data
+    :return: Content from reponse, else None
+    """
     try:
-        response = client.chat.completions.create(
+        response = botclient().chat.completions.create(
             # model="deepseek-reasoner", 
             model="gpt-4o-mini",
             messages=[
@@ -17,9 +32,15 @@ def make_api_request(client, user_input):
             max_tokens=500
         )
         text =  response.choices[0].message.content.strip()
-        return text
+        return text, None
+    except openai.APIConnectionError as e:
+        return None, e
+    except openai.RateLimitError as e:
+        return None, e
     except openai.APIStatusError as e:
-        return None
+        return None, e
+    except openai.OpenAIError as e:
+        return None, e
 
 
 def upload_file_to_s3(data, bucket, object_name):
